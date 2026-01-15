be351f4052ac79220dd0eedfc3c62f909185ac0d
jlbuild <juliabuildbot@gmail.com>
2026-01-15 19:22:00 +0000

    MariaDB_Connector_C_jll build 3.4.8+0

--- src/MariaDB_Connector_C_jll.jl
+++ src/MariaDB_Connector_C_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("MariaDB_Connector_C")
-JLLWrappers.@generate_main_file("MariaDB_Connector_C", UUID("aabc7e14-95f1-5e66-9f32-aea603782360"))
+JLLWrappers.@generate_main_file("MariaDB_Connector_C", Base.UUID("aabc7e14-95f1-5e66-9f32-aea603782360"))
 end  # module MariaDB_Connector_C_jll
